ea76250354c90296ad5f6b73469397d8c39afd9b
kitsuyui <kitsuyui@kitsuyui.com>
2026-03-30 22:35:55 +0900

    Use esbuild preprocessor for Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,11 @@
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
 import codeCoverageTask from '@cypress/code-coverage/task'
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      on('file:preprocessor', createBundler())
       codeCoverageTask(on, config)
       return config
     },
